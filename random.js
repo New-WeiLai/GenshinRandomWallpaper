354c5c5b2b7d02db1ab01475259b3582f7bd8f3a
New-Weilai <future20152024@outlook.com>
2025-12-21 13:34:11 +0800

    Modified some files and Update README.md

---
+++ random.js
@@ -0,0 +1,2 @@
+var r=Math.ceil(Math.random()*166);
+document.write("<img src=/images/PC/"+[r]+".png>")
---
+++ randomphone.js
@@ -0,0 +1,2 @@
+var r=Math.ceil(Math.random()*35);
+document.write("<img src=/images/Phone/"+[r]+".png>")
